d3167c80168a4cd432d81948e19053c917ecf9de
Guru Saran M <gurusaranm0025@gmail.com>
2023-12-12 15:54:45 +0530

    fiished working on the logo part of the navabr

---
+++ components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+function Logo() {
+  return (
+    <div>
+      <span className="text-5xl font-logo bg-logo-gradient bg-clip-text text-transparent">
+        BLOOM
+      </span>
+    </div>
+  );
+}
+
+export default Logo;
---
+++ components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+import Logo from "../Logo/Logo";
+function NavBar() {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+}
+
+export default NavBar;
