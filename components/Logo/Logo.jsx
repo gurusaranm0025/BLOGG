c492ac3b1bedd7c55626070e598047244c6cb3c2
Guru Saran M <gurusaranm0025@gmail.com>
2024-02-11 22:14:56 +0530

    fixed the logo bug

--- components/Logo/Logo.jsx
+++ components/Logo/Logo.jsx
@@ -1,19 +1,31 @@
 "use client";
 import { ThemeContext } from "@/common/ContextProvider";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 function Logo({ className }) {
   let { theme } = useContext(ThemeContext);
+
+  const [themedCSS, setThemedCSS] = useState("bg-logo-gradient ");
+
+  useEffect(() => {
+    setThemedCSS(() => {
+      if (theme === "dark") {
+        return "bg-logo-gradient-dark ";
+      } else {
+        return "bg-logo-gradient ";
+      }
+    });
+  }, [theme]);
 
   return (
     <span
       className={
         "text-4xl font-logo bg-clip-text text-transparent " +
-        (theme == "dark" ? "bg-logo-gradient-dark " : "bg-logo-gradient ") +
+        themedCSS +
         className
       }
     >
-      BLOOM
+      BLOGG
     </span>
   );
 }
